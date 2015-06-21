Resolve critical bug in logistic layer. The error derivative with respect to the inputs was calculated incorrectly, and code would throw an error.

Examples/BackPropagation.py
@@ -44,6 +44,8 @@ def run():
     n_epochs = 3
     #number of iterations in each epoch
     n_iter = 10000
+    #learning rate
+    learning_rate = 0.01
     
     rmse = np.zeros(n_epochs*n_iter)
     
@@ -53,13 +55,13 @@ def run():
             y0 = h0.forward(x)
             y1 = h1.forward(y0)
             rmse[e*n_iter + i], dEdy = pr.squared_error(t, y1)
-            dEdx1 = h1.backprop(dEdy)
-            h0.backprop(dEdx1)
+            dEdx1 = h1.backprop(dEdy, learning_rate)
+            h0.backprop(dEdx1, learning_rate)
             
             #print error and weights at end of each iteration
             if(i % 100 == 0):
                 print "RMSE:" + str(rmse[i]) + " w: " + str(h0.w)
-        h0.learning_rate *= 0.1
+        learning_rate *= 0.1
         
     #plot data
     plt.figure(figsize=(20, 9))

logistic.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class logistic:
-    learning_rate = 0.001
+    
     #constructor
     def __init__(self, n_units, n_inputs):
         print 'Initializing logistic layer with ' + str(n_units) + ' units and ' + str(n_inputs) + ' inputs per unit '
@@ -20,7 +20,7 @@ class logistic:
         return self.y
     
     #take error-derivative with respect to output as parameter
-    def backprop(self, dEdy):
+    def backprop(self, dEdy, learning_rate):
         self.dEdy = dEdy
         #estimate dE/dz = dE/dy * dy/dz, where
         #dy/dz = y(1-y)
@@ -32,13 +32,11 @@ class logistic:
         # and sum over all training samples
         dEdw = np.dot(self.x, dEdz.T)
         
-        #sum weights in each dimension over all neurons
-        sumW = sum(self.w.T)
         #dE/dx for each training sample
-        dEdx = np.dot(sumW[0:self.n_inputs,:], dEdz)
+        dEdx = np.dot(self.w[0:self.n_inputs,:], dEdz)
         
         #update weights
-        self.w += self.learning_rate * dEdw
+        self.w += learning_rate * dEdw
         
         return dEdx
         
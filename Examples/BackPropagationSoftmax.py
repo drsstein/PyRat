Add softmax example and fix some minor bugs.

Examples/BackPropagationSoftmax.py
@@ -1,5 +1,18 @@
-# short lecture on backpropagation by Geoffrey Hinton:
-# https://www.youtube.com/watch?v=H47Y7pAssTI
+# short lectures on backpropagation and softmax by Geoffrey Hinton:
+#
+# Backpropagation: https://www.youtube.com/watch?v=H47Y7pAssTI
+#
+# Softmax: https://www.youtube.com/watch?v=yqsI-X40OBY
+#
+# This example is very similar to the BackPropagation example. The key difference
+# is that the last layer is a softmax with two units instead of a single 
+# logistic unit. We train the softmax to represent a probability distribution 
+# over two classes ('pos' and 'neg'). To do that, we need to change the labels
+# from scalar ('1' for examples from the 'pos' class and 0 for 'neg' samples) to
+# vector representation encoding class membership for each class individually.
+# The switch to softmax also allows us to minimize cross-entropy error instead
+# of least squares, which is a better fit for classification problems as explained
+# in the softmax-video linked above.
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,50 +31,50 @@ def run():
     m_pos2 = np.array([0.5, 0.5]).reshape(2, -1)
     x_pos2 = np.random.randn(2, n/2)/4 + m_pos2
     x_pos = np.concatenate((x_pos1, x_pos2), axis=1)
-    y_pos = np.ones(n)
+    ones = np.ones(n)
+    zeros = np.zeros(n)
+    y_pos = np.vstack((ones, zeros))
     
     m_neg1 = np.array([-0.5, 0.5]).reshape(2, -1)    
     x_neg1 = np.random.randn(2, n/2)/4 + m_neg1
     m_neg2 = np.array([0.5, -0.5]).reshape(2, -1)    
     x_neg2 = np.random.randn(2, n/2)/4 + m_neg2
     x_neg = np.concatenate((x_neg1, x_neg2), axis=1)
-    y_neg = np.zeros(n)
+    y_neg = np.vstack((zeros, ones))
 
     #concatenate positive and negative examples
     x = np.concatenate((x_pos, x_neg), axis=1)
     y = np.concatenate((y_pos, y_neg))
     
     #set y as 't'arget
-    t = y.reshape(1, -1)
+    t = y.reshape(2, -1)
     
     #initialize hidden layer with 2 logistic units
     n_dims = len(x)
     n0 = 4
     h0 = pr.logistic(n0, n_dims)
     #initialize output layer with 1 logistic unit
-    h1 = pr.logistic(1, h0.n_outputs)
+    h1 = pr.softmax(2, n0)
     
     #define number of training epochs (decreasing learning rate)
-    n_epochs = 4
+    n_epochs = 1
     #number of iterations in each epoch
-    n_iter = 1000
+    n_iter = 5000
     #learning rate
-    learning_rate = 10.0
+    learning_rate = 1.
     
-    rmse = np.zeros(n_epochs*n_iter)
+    cost = np.zeros(n_epochs*n_iter)
     
     #train network
     for e in range(n_epochs):
         for i in range(n_iter):
             y0 = h0.forward(x)
-            y1 = h1.forward(y0)
-            rmse[e*n_iter + i], dEdy = pr.squared_error(t, y1)
-            dEdx1 = h1.backprop(dEdy, learning_rate)
+            cost[e*n_iter + i], dEdx1 = h1.evaluate(y0, t, learning_rate)
             h0.backprop(dEdx1, learning_rate)
             
             #print error and weights at end of each iteration
             if(i % 1000 == 0):
-                print "RMSE:" + str(rmse[e * n_iter + i]) + " w: " + str(h0.w)
+                print "Cost:" + str(cost[e*n_iter+i]) + " w: " + str(h0.w)
         learning_rate *= 0.1
         
     #plot data
@@ -74,13 +87,13 @@ def run():
     #separation line at p=0.5 is defined as x2 = -w1/w2 * x1 - w0/w2
     w = h0.w
     l_x = np.linspace(-1, 1, 2)
-    for i in range(n0):
+    for i in range(h0.n_outputs):
         l_y = -w[0,i]/w[1,i] * l_x -w[n_dims,i]/w[1,i]
         plt.plot(l_x, l_y, color='black')
     
     #plot mean error over iterations
     e_x = np.linspace(1, n_epochs*n_iter, n_epochs*n_iter)
     plt.subplot(122)
-    plt.title('Root mean squared error')
-    plt.plot(e_x, rmse)
+    plt.title('Cross-entropy error')
+    plt.plot(e_x, cost)
     plt.show()

logistic.py
@@ -7,7 +7,7 @@ class logistic:
         print 'Initializing logistic layer with ' + str(n_units) + ' units and ' + str(n_inputs) + ' inputs per unit '
         self.n_outputs = n_units
         self.n_inputs = n_inputs
-        self.w = np.random.rand(self.n_inputs+1, self.n_outputs)/self.n_inputs - 0.5
+        self.w = (np.random.rand(self.n_inputs+1, self.n_outputs)- 0.5)/n_inputs 
     
     #take data from previous layer as input
     def forward(self, x):
@@ -30,7 +30,8 @@ class logistic:
         #estimate dE/dw = dE/dy * dy/dz * dz/dw, where
         # - dz/dw = x and
         # and sum over all training samples
-        dEdw = np.dot(self.x, dEdz.T)
+        n_samples = dEdz.shape[1]
+        dEdw = np.dot(self.x, dEdz.T)/n_samples
         
         #dE/dx for each training sample
         dEdx = np.dot(self.w[0:self.n_inputs,:], dEdz)

softmax.py
@@ -8,7 +8,7 @@ class softmax:
     def __init__(self, n_units, n_inputs):
         self.n_outputs = n_units
         self.n_inputs = n_inputs
-        self.w = np.random.rand(self.n_inputs+1, self.n_outputs)/self.n_inputs - 0.5
+        self.w = (np.random.rand(self.n_inputs+1, self.n_outputs)- 0.5)/self.n_inputs 
     
     #as the softmax is always the last layer in the network, and the cross-entropy
     #error can be easily computed with respect to the logit (y - t), we do both
@@ -25,12 +25,14 @@ class softmax:
         self.y /= sum(self.y)
         
         #estimate error derivative
-        dEdz = self.y - targets
+        dEdz = targets - self.y
         #estimate cross entropy error across samples
-        C = sum(-sum(targets*np.log(self.y)))
+        #it is important to normalize here wrt. the number of training cases
+        n_samples = dEdz.shape[1]
+        C = sum(-sum(targets*np.log(self.y)))/n_samples
         
         #propagate backwards through weights and inputs
-        dEdw = np.dot(self.x, dEdz.T)
+        dEdw = np.dot(self.x, dEdz.T)/n_samples
         
         #dE/dx for each training sample
         dEdx = np.dot(self.w[0:self.n_inputs,:], dEdz)